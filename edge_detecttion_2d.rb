f7c5fec14a16129e827478653cc632d920621ddf
zeeshangulzar <xeeshangulzar@gmail.com>
2017-05-03 13:38:01 +0500

    Added all validations

--- edge_detecttion_2d.rb
+++ edge_detecttion_2d.rb
@@ -1,4 +1,6 @@
 input = []
+output = []
+results = []
 
 #file reading
 File.open("cat.txt", "r") do |f|
@@ -8,7 +10,7 @@ File.open("cat.txt", "r") do |f|
   end
 end
 
-#validations
+#empty file and irregular input validation
 flags = input.map {|array| array.size}.uniq
 if flags.size ==  0 #blank file validation
   puts "File is empty, Please provide valid ASCII encoded string file."
@@ -16,14 +18,34 @@ if flags.size ==  0 #blank file validation
 elsif flags.count != 0 && flags.count > 1 #irregular size validation
     puts "Please enter same size of ASCII encoded string"
     exit
-else
-  input.each do |array|
-    binary_flags = array.map {|v| v =='0' || v=='1' }.uniq
-    if binary_flags.count == 1 && binary_flags.first == true
-      #your code  here
-    else
-      puts "Please provide valid ASCII encoded file"
-    end
-  end
-
 end
+
+#non binary input validation
+binary_flag = false
+input.each do |array|
+  binary_flags =  array.map {|v| v =='0' || v=='1' }.uniq
+  if !(binary_flags.count == 1 && binary_flags.first == true)
+    puts "Please provide valid ASCII encoded file"
+    exit
+  end
+end
+
+
+puts "working code"
+
+# input.each_with_index do |values, index|
+#   if index == 0
+#     inner_output = ['0']
+#     values.each_with_index do |value, inner_index|
+#       if value[inner_index + 1] != value[inner_index]
+#         inner_output << '1'
+#       else
+#         inner_output << '0'
+#       end
+#     end
+#     output << inner_output
+#   else
+#   end
+# end
+# results = output.map { |array| array.join }
+# puts "Output: #{results}"
